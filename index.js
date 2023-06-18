05c849c6254399cc4e92f737b812f867f5dfd333
wutchara <sstmuham@gmail.com>
2023-06-18 18:42:30 +0700

    Update solution

--- index.js
+++ index.js
@@ -2,6 +2,47 @@
  * @param {number} n
  * @return {string[]}
  */
+
+
+
+// Runtime => 74 ms, Memory => 44.2 MB
+// var fizzBuzz = function(n) {
+//     result = [];
+//     for (let i = 1; i <= n; i++) {
+//         tmp = "";
+//         if (i % 3 === 0) {
+//             tmp += "Fizz";
+//         }
+        
+//         if (i % 5 === 0) {
+//             tmp += "Buzz";
+//         }
+
+//         result.push(!tmp ? `${i}` : tmp);
+//     }
+
+//     return result;
+// };
+
+// Runtime => 64 ms, Memory => 45 MB
 var fizzBuzz = function(n) {
-    
+    let fizzIndex = 3;
+    let buzzIndex = 5;
+    result = [];
+    for (let i = 1; i <= n; i++) {
+        tmp = "";
+        if (i === fizzIndex) {
+            tmp += "Fizz";
+            fizzIndex += 3;
+        }
+        
+        if (i === buzzIndex) {
+            tmp += "Buzz";
+            buzzIndex += 5;
+        }
+
+        result.push(!tmp ? `${i}` : tmp);
+    }
+
+    return result;
 };
